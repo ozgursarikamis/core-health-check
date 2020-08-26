506adad83868c1c4d71d2bd38e5b4d9398570bed
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-26 21:40:09 +0300

    Add project files.

---
+++ InvestmentManager/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using InvestmentManager.Core;
+using InvestmentManager.DataAccess.EF;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+namespace InvestmentManager
+{
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment env, IConfiguration configuration, ILoggerFactory loggerFactory)
+        {
+            Configuration = configuration;
+            this.loggerFactory = loggerFactory;
+
+            // For NLog                   
+            NLog.LogManager.LoadConfiguration("nlog.config");
+        }
+
+        public IConfiguration Configuration { get; }
+
+        private readonly ILoggerFactory loggerFactory;
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddSingleton<IConfiguration>(this.Configuration);
+
+            // Configure the data access layer
+            var connectionString = this.Configuration.GetConnectionString("InvestmentDatabase");
+
+            services.RegisterEfDataAccessClasses(connectionString, loggerFactory);  
+
+            // For Application Services
+            String stockIndexServiceUrl = this.Configuration["StockIndexServiceUrl"];
+            services.ConfigureStockIndexServiceHttpClientWithoutProfiler(stockIndexServiceUrl);
+            services.ConfigureInvestmentManagerServices(stockIndexServiceUrl);
+
+            // Configure logging
+            services.AddLogging(loggingBuilder => {
+                loggingBuilder.AddConsole();
+                loggingBuilder.AddDebug();
+                loggingBuilder.AddNLog();
+            });
+        }
+
+
+        // Configures the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseExceptionHandler("/Home/Error");
+            
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
